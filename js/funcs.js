cb5cdb5c56a4e2e2f11fdcad65cb09a29738f0cf
Brian Crumley <brian@o3world.com>
2014-04-04 10:43:49 -0400

    initial push of workshop build files and assets

---
+++ js/funcs.js
@@ -0,0 +1,54 @@
+/*
+ * RWD Workshop
+ * 
+ * Custom Vanilla Javascript and jQuery Functions
+ *  
+ * Changelog:
+ * 
+ ************************************************************************************************/
+
+
+// Avoid `console` errors in browsers that lack a console... I'm looking at you IE
+if ( !( window.console && console.log ) ) {
+    ( function( ) {
+        var noop = function( ) { };
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = { };
+        while ( length-- ) {
+            console[ methods[ length ] ] = noop;
+        };
+    } ( ) );
+};
+
+
+/*
+ * Global Variables
+ *
+ ************************************************************************************************/
+
+
+
+/*
+ * Custom Functions
+ *
+ ************************************************************************************************/
+
+
+
+/*
+ * jQuery Actions
+ *
+ ************************************************************************************************/
+
+
+
+/*
+ * jQuery DOM Ready
+ *
+ ************************************************************************************************/
+
+
+jQuery( document ).ready( function( ) {
+	
+} );
